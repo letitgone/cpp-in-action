7760379cb74b504b29bc9a917790d65d1f491e05
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2021-08-16 07:47:04 +0800

    Gavin:Row

--- main.cpp
+++ main.cpp
@@ -2,5 +2,7 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << R"(Jim "King" Tutt uses "\n" instead of endl.)" << '\n';
+    std::cout << R"+*("(Who wouldn't?)", she whispered.)+*" << std::endl;
     return 0;
 }
